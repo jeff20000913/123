d5da8d3b343d2e613f85f11d92accfd0ef36b31c
tcyang <tcyang@gm.pu.edu.tw>
2021-03-26 11:33:18 +0800

    用手勢切換圖片

--- app/src/main/java/tw/edu/pu/csim/tcyang/pu/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/pu/MainActivity.kt
@@ -13,6 +13,18 @@ class MainActivity : AppCompatActivity(),
 
     lateinit var gDetector: GestureDetector
 
+    var PictureNo:Int = 0  //目前顯示第幾張圖
+    var TotalPictures:Int = 4 //總共幾張圖片(假設僅顯示pu0-pu3)
+
+    fun ShowPicture(){
+        when (PictureNo){
+            0 -> img.setImageResource(R.drawable.pu0)
+            1 -> img.setImageResource(R.drawable.pu1)
+            2 -> img.setImageResource(R.drawable.pu2)
+            3 -> img.setImageResource(R.drawable.pu3)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -35,33 +47,50 @@ class MainActivity : AppCompatActivity(),
  */
 
     override fun onDown(p0: MotionEvent?): Boolean {
-        txv.text = "按下"
+        //txv.text = "按下"
         return true
     }
 
     override fun onShowPress(p0: MotionEvent?) {
-        txv.text = "按下後無後續動作"
+        //txv.text = "按下後無後續動作"
     }
 
     override fun onSingleTapUp(p0: MotionEvent?): Boolean {
-        txv.text = "短按"
+        //txv.text = "短按"
+        PictureNo = 0
+        ShowPicture()
         return true
     }
 
     override fun onScroll(e1: MotionEvent, e2: MotionEvent, distanceX: Float, distanceY: Float): Boolean {
+        /*
         txv.text = "拖曳\nx1y1: " +  e1.getX().toString() + ", " + e1.getY().toString() +
                 "\nx2y2: " + e2.getX().toString() + ", " + e2.getY().toString()
+        */
         return true
     }
 
     override fun onLongPress(p0: MotionEvent?) {
-        txv.text = "長按"
+        //txv.text = "長按"
+        PictureNo = TotalPictures - 1
+        ShowPicture()
     }
 
     override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
+        /*
         txv.text = "快滑\nx1y1: " + e1.getX().toString() + ", " + e1.getY().toString() +
                 "\nx2y2: " + e2.getX().toString() + ", " + e2.getY().toString() +
                 "\nX軸Y軸速度:" + velocityX.toString() + ", " +  velocityY.toString()
+         */
+        if (e1.getX() < e2.getX()){  //向右快滑
+            PictureNo++
+            if (PictureNo == TotalPictures) {PictureNo = 0}
+        }
+        else{     //向左快滑
+            PictureNo--;
+            if (PictureNo < 0) {PictureNo = TotalPictures - 1 }
+        }
+        ShowPicture()
         return true
     }
 
@@ -74,7 +103,7 @@ class MainActivity : AppCompatActivity(),
     }
 
     override fun onDoubleTapEvent(p0: MotionEvent?): Boolean {
-        txv.text = "連續點兩下"
+        //txv.text = "連續點兩下"
         return true
     }
 
